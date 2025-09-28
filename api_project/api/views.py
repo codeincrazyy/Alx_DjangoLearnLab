8b7d63bc524418872849dde5a5fb2dd1b8e43881
Abigail <abigailtrycode@gmail.com>
2025-09-28 19:07:48 +0300

    done

--- api_project/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
-
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()       # fetches all Book objects
-    serializer_class = BookSerializer   # serializes them into JSON
--- api_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include   # include is important
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   # now Django knows to look inside api/urls.py
-]
