65c5b0495da728ed62efa73610aecc321b459bd6
Abigail <abigailtrycode@gmail.com>
2025-10-09 21:14:54 +0300

    done with Authentication

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,54 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from .models import CustomUser
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
+from django.contrib.auth import authenticate
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+       
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'user': serializer.data, 'token': token.key})
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user_id': user.pk, 'username': user.username})
+class loginView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'username': user.username})
+        return Response({'error': 'Invalid credentials'}, status=400)
+
+
+# Profile View
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+    
