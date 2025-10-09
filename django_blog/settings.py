210e8a1a90d22bf24674016e7c0e0c3a7e03b0b6
abiagil <abigailtrycode@gmail.com>
2025-10-09 19:05:06 +0300

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -39,9 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "taggit",
-    "USER",
-    "PORT",
-    
 ]
 
 MIDDLEWARE = [
@@ -80,8 +77,12 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
