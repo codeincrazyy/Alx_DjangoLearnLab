219dbc3a8700ed4101fb30abfe1dcd6118e77af5
Abigail <abigailtrycode@gmail.com>
2025-10-09 18:45:34 +0300

    fixed model.py

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    ublished_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
     tags = TaggableManager(blank=True) 
 
     def __str__(self):
