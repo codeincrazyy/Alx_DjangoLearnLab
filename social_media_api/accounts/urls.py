4b1a9d67645af3ecbc9cae46b5163ec4bcc629f1
Abigail <abigailtrycode@gmail.com>
2025-10-12 14:36:13 +0300

    implemented User Follows and Feed Functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, CustomAuthToken, ProfileView,UserViewSet
+from .views import RegisterView, CustomAuthToken, ProfileView,UserViewSet,follow_user, unfollow_user 
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('', include(router.urls)),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
 ]
