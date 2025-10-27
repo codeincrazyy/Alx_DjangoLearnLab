83ca4f82005eeed00de6c85be55e1f0a4a1f30a4
Abigail <abigailtrycode@gmail.com>
2025-10-28 00:51:53 +0300

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Library, Book
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Function-based view to list all books
 def book_list(request):
     books = Book.objects.all()
