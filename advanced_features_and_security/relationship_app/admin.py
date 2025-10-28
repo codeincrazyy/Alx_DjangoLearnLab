f59422ec9a6dfa0db8f6188e100f458df472d160
Abigail <abigailtrycode@gmail.com>
2025-10-28 16:01:06 +0300

    done

--- advanced_features_and_security/relationship_app/admin.py
+++ advanced_features_and_security/relationship_app/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register the custom user model with the admin
+admin.site.register(CustomUser, CustomUserAdmin)
